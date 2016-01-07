a0acfd60a92f2bae5ff08a84fb841621a986fa08
franciscominajas <franciscominajas@outlook.com>
2016-01-06 20:41:42 -0600

    Se adicionaron las clases para el manejo de metodos de la camara, asi como la obtencion de los frames correspondientes

---
+++ app/src/main/java/com/example/franciscominajas/trabajoterminal/Camara.java
@@ -0,0 +1,130 @@
+package com.example.franciscominajas.trabajoterminal;
+
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+/**
+ * Created by FRANCISCOMINAJAS on 03/01/2016.
+ */
+public class Camara implements SurfaceHolder.Callback, Camera.PreviewCallback
+{
+    //variables
+    private Camera camara = null;
+    private ImageView previzualizacionCamara = null;
+    private Bitmap bitmap = null;
+    private int[] pixeles = null;
+    private byte[] informacionFrames = null;
+    private int formatoImagen;
+    private int ancho;
+    private int alto;
+    private boolean procesando = false;
+
+    //inicializar el manipulador de procesos
+    Handler handler = new Handler(Looper.getMainLooper());
+
+    public Camara(int Ancho, int Alto, ImageView previsualizacion)
+    {
+        ancho = Ancho;
+        alto = Alto;
+        previzualizacionCamara = previsualizacion;
+        //Bitmap.Config.ARGB_8888 Cada pixel almacena 4 bytes.
+        bitmap = Bitmap.createBitmap(ancho, alto, Bitmap.Config.ARGB_8888);
+        //creamos un arreglo donde almacenaremos todos los pixeles.
+        pixeles = new int[alto*ancho];
+    }
+
+    public void onPreviewFrame(byte[] arg0, Camera arg1)
+    {
+        //en modo de previsualizacion, cada frame sera colocado aqui
+        //ImageFormat.NV21 YCrCb formato utilizado para las imágenes, que utiliza el formato de codificación NV21.
+        if(formatoImagen == ImageFormat.NV21)
+        {
+            //solamente aceptamos el formato NV21(YUV420).
+            if(!procesando)
+            {
+                informacionFrames=arg0;
+                handler.post(DoImageProcessing);
+            }
+        }
+    }
+
+    public void onPause()
+    {
+        camara.stopPreview();
+    }
+
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3)
+    {
+        Parameters parametros;
+        //inicializamos el tamaño de previsualizacion
+        parametros=camara.getParameters();
+        parametros.setPreviewSize(ancho, alto);
+        formatoImagen=parametros.getPreviewFormat();
+        camara.setParameters(parametros);
+        camara.startPreview();
+    }
+
+    public void surfaceCreated(SurfaceHolder arg0)
+    {
+        camara = Camera.open();
+        try
+        {
+            //si no colocamos el SurfaceHolder, la area de previsualizacion sera negra.
+            camara.setPreviewDisplay(arg0);
+            camara.setPreviewCallback(this);
+        }
+        catch(IOException e)
+        {
+            camara.release();
+            camara = null;
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder arg0)
+    {
+        camara.setPreviewCallback(null);
+        camara.stopPreview();
+        camara.release();
+        camara = null;
+    }
+
+    public boolean procesamientoImagen(int Ancho, int Alto, byte[] NV21FrameData, int[] pixels)
+    {
+        //zona de procesamiento
+        return true;
+    }
+
+    //hilo
+    public Runnable DoImageProcessing = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            Log.i("Prototipo_1","Procesando");
+            procesando=true;
+            procesamientoImagen(ancho, alto, informacionFrames, pixeles);
+
+            bitmap.setPixels(pixeles, 0, ancho, 0, 0, ancho, alto);
+
+            previzualizacionCamara.setImageBitmap(bitmap);
+            procesando = true;
+        }
+    };
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+}
--- app/src/main/java/com/example/franciscominajas/trabajoterminal/MainActivity.java
+++ app/src/main/java/com/example/franciscominajas/trabajoterminal/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.franciscominajas.trabajoterminal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity
+{
+    //inicializacion de variables
+    private Camara prevCamara;
+    private ImageView previsualizacionCam = null;
+    private FrameLayout principalLayout;
+    private int ancho = 640;
+    private int alto = 480;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        //colocamos la apk en full screen opcional
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //colocamos la apk sin titulo
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        //creamos la inicializacion de la camara
+        previsualizacionCam  = new ImageView(this);
+
+        SurfaceView vistaCam = new SurfaceView(this);
+        SurfaceHolder HolderCam = vistaCam.getHolder();
+        prevCamara = new Camara(ancho, alto, previsualizacionCam);
+
+        HolderCam.addCallback(prevCamara);
+        HolderCam.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        principalLayout = (FrameLayout) findViewById(R.id.layout1);
+        principalLayout.addView(vistaCam, new LayoutParams(ancho, alto));
+        principalLayout.addView(previsualizacionCam, new LayoutParams(ancho, alto));
+    }
+    protected void onPause()
+    {
+        if(prevCamara!=null)
+        {
+            prevCamara.onPause();
+        }
+        super.onPause();
     }
 }
